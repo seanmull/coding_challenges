511dad81e8f99f1b998b4a3e909f795b503e6257
sean.mull <kimlan065@gmail.com>
2024-05-16 14:47:28 +1000

    basic functionality for json parser.

--- json_parser.py
+++ json_parser.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+import pprint
 
 # Good description on how to use argparse https://www.bitecode.dev/p/parameters-options-and-flags-for
 
@@ -16,6 +18,48 @@ file = open(args.filename, 'r')
 
 content = file.read()
 
-# Figure out how build a lexer and parser
+# TODO need to figure out a way to allow the comma to be missing in the end only
+regex = '{(\s*"[\w-]*"\s*:{1}\s*("\w*"|\[\s*\]|true|false|null|\{\s*\}|\d*)\s*,\n*)*}'
 
-print("")
+match = re.match(regex, content)
+
+if match:
+    print("The json file is valid.")
+else:
+    print("The json file is invalid.")
+    exit()
+
+obj = {}
+
+stripedcontent = content.strip().strip("{}")
+
+stripedcontentarr = stripedcontent.split(",")
+
+if len(stripedcontentarr) == 1 and stripedcontentarr[0] == "":
+    print("{}")
+else:
+    for ele in stripedcontentarr:
+        if ele == "\n":
+            continue
+        key, value = ele.split(":")
+        key = key.strip()
+        value = value.strip()
+        if value == "null":
+            value = None
+        elif value == "true":
+            value = True
+        # need to capture possiblity that whitespace is between brackets
+        elif value == "[]":
+            value = []
+        elif value == "{}":
+            value = {}
+        elif value == "false":
+            value = False
+        else:
+            try:
+                value = int(value)
+            except ValueError:
+                print(value + " is not a number.")
+        obj[key] = value
+
+pprint.pp(obj)
