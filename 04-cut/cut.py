65a3b28d80bebafc00d11b4652e4be4a2b5cff23
sean.mull <kimlan065@gmail.com>
2024-05-21 14:09:20 +1000

    added funtionality for multiple fields

--- 04-cut/cut.py
+++ 04-cut/cut.py
@@ -5,36 +5,51 @@ import argparse
 parser = argparse.ArgumentParser(description="Cuts out portions of each line from file")
 
 
-parser.add_argument(
-        "filename", 
-        type=str, 
-        nargs="?", 
-        help="File you want to readin.")
+parser.add_argument("filename", type=str, nargs="?", help="File you want to readin.")
 
 parser.add_argument(
     "-f",
     "--field",
-    type=str, 
     help="Which field we want",
+)
+
+parser.add_argument(
+    "-d",
+    "--delimiter",
+    type=str,
+    nargs="?",
+    default="\t",
+    help="What delimiter you need",
 )
 
 
 args = parser.parse_args()
 
 if args.filename:
-    file = open(args.filename, 'r')
+    file = open(args.filename, "r")
 else:
     import sys
+
     file = sys.stdin
 
 content = file.read()
+
+if "," in args.field:
+    args.field = args.field.split(",")
+elif " " in args.field:
+    args.field = args.field.split(" ")
 
 lines = content.split("\n")
 
 table = []
 for line in lines:
-    tabs = line.split("\t")
+    tabs = line.split(args.delimiter)
     table.append(tabs)
-    print(tabs[int(args.field) - 1])
+    for i, field in enumerate(args.field):
+        if i == len(args.field) - 1:
+            print(tabs[int(field) - 1], end="")
+        else:
+            print(tabs[int(field) - 1] + args.delimiter, end="")
+    print()
 
-
+print("hello")
