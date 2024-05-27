38a38b182246bd85e94a4b755d2ff870b60b5253
sean.mull <kimlan065@gmail.com>
2024-05-27 13:18:47 +1000

    have basic functionality to store key values of redis server

--- 08-redis/client.py
+++ 08-redis/client.py
@@ -1,7 +1,7 @@
 import aiohttp
 import asyncio
 import argparse
-import utils
+from utils import serialize_resp
 
 # Good description on how to use argparse https://www.bitecode.dev/p/parameters-options-and-flags-for
 
@@ -9,15 +9,22 @@ parser = argparse.ArgumentParser(description="Sends requests to redis server")
 
 parser.add_argument("cmd", type=str, help="cmd")
 
-parser.add_argument("data", type=str, nargs="?", help="data")
+parser.add_argument("key", type=str, nargs="?", help="data")
+
+parser.add_argument("value", type=str, nargs="?", help="data")
 
 args = parser.parse_args()
 
 
-async def fetch(session, url, cmd, data):
-    if not data:
-        data = 0
-    params = {"cmd": cmd, "data": data}
+async def fetch(session, url, cmd, key, value):
+    bulk_array = []
+    if not key and not value:
+        bulk_array = [cmd]
+    elif not value:
+        bulk_array = [cmd, key]
+    else:
+        bulk_array = [cmd, key, value]
+    params = {"request": serialize_resp(bulk_array)}
     async with session.get(url, params=params) as response:
         return await response.text()
 
@@ -26,7 +33,7 @@ async def main():
     url = "http://localhost:6379"
 
     async with aiohttp.ClientSession() as session:
-        response = await fetch(session, url, args.cmd, args.data)
+        response = await fetch(session, url, args.cmd, args.key, args.value)
         print(response)
 
 
--- 08-redis/server.py
+++ 08-redis/server.py
@@ -1,15 +1,37 @@
 import aiohttp
 from aiohttp import web
+from utils import deserialize_resp
+
+cache = {}
 
 async def handle(request):
-    cmd = request.rel_url.query.get('cmd')
-    data = request.rel_url.query.get('data')
-    if cmd == "PING":
-        text = "PONG"
-    elif cmd == "ECHO":
-        text = f"{data}"
-    else:
-        text = f"{cmd}, {data}"
+    serialized_request = request.rel_url.query.get('request')
+    request = deserialize_resp(serialized_request)
+    cmd, key, value, text = "", "", "", ""
+    if len(request) == 3:
+        cmd, key, value = request
+        if cmd == "set":
+            cache[key] = value
+            print(f"{key} is set to {value}.")
+    elif len(request) == 2:
+        cmd, key = request
+        if cmd == "ECHO":
+            text = f"{key}"
+        elif cmd == "get":
+            if cache.get(key):
+                pass
+                text = f"{cache[key]}"
+            else:
+                print(f"{key} is not in cache.")
+        else:
+            text = f"{cmd}, {key}"
+    elif len(request) == 1:
+        cmd = request
+        if cmd[0] == "PING":
+            text = "PONG"
+        else:
+            text = f"{cmd[0]}"
+    print(f"cache is {cache}")
     return web.Response(text=text)
 
 app = web.Application()
