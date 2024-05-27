86c9c04d8a90c0ac7a71473929c889b3a80165b9
sean.mull <kimlan065@gmail.com>
2024-05-27 14:10:34 +1000

    running concurrency test

---
+++ 08-redis/concurrency.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
+python client.py set hello world & 
+python client.py get hello &
