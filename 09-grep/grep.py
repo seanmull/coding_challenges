2d04b62a2b21908afde67215d509ea26e4cf0870
sean.mull <kimlan065@gmail.com>
2024-05-30 12:40:54 +1000

    added case insensivitivy to grep function

--- 09-grep/grep.py
+++ 09-grep/grep.py
@@ -33,23 +33,35 @@ parser.add_argument(
     help="Inverts the find",
 )
 
+parser.add_argument(
+    "-i",
+    "--insensitive",
+    action="store_true",
+    help="Case insensitive search",
+)
+
 args = parser.parse_args()
 
-def grep(file_path, regex_pattern, invert):
+def grep(file_path, regex_pattern, invert, insensitive):
     # Compile the regular expression pattern
-    pattern = re.compile(regex_pattern)
+    if insensitive and invert:
+        regex_pattern = rf'(?:(?!{regex_pattern}).)*$'
+        pattern = re.compile(regex_pattern, re.IGNORECASE)
+    elif insensitive and not invert:
+        pattern = re.compile(regex_pattern, re.IGNORECASE)
+    elif not insensitive and invert:
+        regex_pattern = rf'(?:(?!{regex_pattern}).)*$'
+        pattern = re.compile(regex_pattern)
+    else:
+        pattern = re.compile(regex_pattern)
     
     matching_lines = []
     
     # Open the file and read it line by line
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
-            if invert:
-                if not pattern.search(line):
-                    matching_lines.append(line.rstrip() + '\n')  # Remove trailing whitespace and ensure a single newline
-            else:
-                if pattern.search(line):
-                    matching_lines.append(line.rstrip() + '\n')  # Remove trailing whitespace and ensure a single newline
+            if pattern.search(line):
+                matching_lines.append(line.rstrip() + '\n')  # Remove trailing whitespace and ensure a single newline
 
     # Join the matching lines back into a single string
     result = ''.join(matching_lines)
@@ -60,13 +72,14 @@ if __name__ == "__main__":
     file_path = args.filename
     regex_pattern = args.regex
     invert = args.invert
+    insensitive = args.insensitive
 
     if args.recursive:
         files = list_files_in_folder(file_path)
         for file in files:
-            matched_content = grep(file, regex_pattern, invert)
+            matched_content = grep(file, regex_pattern, invert, insensitive)
             print(str(Path.cwd()) + file)
             print(matched_content, end='')  # Print without adding extra newlines
     else:
-        matched_content = grep(file_path, regex_pattern, invert)
+        matched_content = grep(file_path, regex_pattern, invert, insensitive)
         print(matched_content, end='')  # Print without adding extra newlines
