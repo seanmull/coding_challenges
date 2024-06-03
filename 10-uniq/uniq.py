89d2b8dbb87fe0aeb71a37a578d26d855a390746
sean.mull <kimlan065@gmail.com>
2024-06-03 13:34:30 +1000

    added functionality for uniq and repeatable for uniq tool

--- 10-uniq/uniq.py
+++ 10-uniq/uniq.py
@@ -11,6 +11,27 @@ parser.add_argument(
         nargs="?", 
         help="File you want to readin.")
 
+parser.add_argument(
+    "-c",
+    "--count",
+    action="store_true",
+    help="provides a column that shows how many repeats of each line",
+)
+
+parser.add_argument(
+    "-d",
+    "--repeated",
+    action="store_true",
+    help="Only shows the repeated lines.",
+)
+
+parser.add_argument(
+    "-u",
+    "--unique",
+    action="store_true",
+    help="Only shows the repeated lines.",
+)
+
 args = parser.parse_args()
 
 if args.filename:
@@ -23,9 +44,35 @@ content = file.read()
 
 lines = content.split("\n")
 
-cache = set()
+cache = {}
 
 for line in lines:
+    if len(line) == 0:
+        continue
     if line not in cache:
-        print(line)
-    cache.add(line)
+        cache[line] = 1
+    else:
+        cache[line] += 1
+
+start = 0
+end = len(cache.items())
+
+for key, value in cache.items():
+    if start >= end:
+        break
+    if args.repeated:
+        if args.count and (value > 1):
+            print (f"{value}\t{key}")
+        elif value > 1:
+            print (f"{key}")
+    elif args.unique:
+        if args.count and (value == 1):
+            print (f"{value}\t{key}")
+        elif value == 1:
+            print (f"{key}")
+    else:
+        if args.count:
+            print (f"{value}\t{key}")
+        else:
+            print (f"{key}")
+    start += 1
