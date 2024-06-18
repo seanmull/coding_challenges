e78c043b8805497af8e01aea39337f6549420ddc
sean.mull <kimlan065@gmail.com>
2024-06-18 13:13:16 +1000

    added expiry and some tests.  Some issue with expiry on testing expired keys

--- 17-memcache/server.py
+++ 17-memcache/server.py
@@ -1,15 +1,8 @@
 import socket
 import threading
+import time
 
 cache = {}
-
-def handle_get_request(connection, key):
-    if key in cache:
-        value = cache[key]
-        response = f"VALUE {key} {value['flags']} {value['byte_count']}\r\n{value['data_block']}\r\nEND\r\n"
-        connection.sendall(response.encode('utf-8'))
-    else:
-        connection.sendall(b"END\r\n")
 
 def handle_client(connection):
     try:
@@ -27,8 +20,7 @@ def handle_client(connection):
                 if command_parts[0] == 'get' and len(command_parts) == 2:
                     key = command_parts[1]
                     handle_get_request(connection, key)
-                    buffer = remainder
-                    continue
+                    break
 
                 if len(command_parts) < 5:
                     print("Incomplete command line")
@@ -36,7 +28,7 @@ def handle_client(connection):
                     continue
 
                 byte_count = int(command_parts[4])
-                expected_length = byte_count + 2  # 2 for the trailing \r\n
+                expected_length = byte_count + 2
 
                 if len(remainder) >= expected_length:
                     data_block = remainder[:byte_count]
@@ -47,39 +39,70 @@ def handle_client(connection):
                     value = {
                         "command_name": command_parts[0],
                         "flags": command_parts[2],
-                        "exptime": command_parts[3],
+                        "exptime": int(command_parts[3]),
                         "byte_count": command_parts[4],
                         "data_block": data_block,
                         "noreply": len(command_parts) == 6 and command_parts[5].lower() == 'noreply'
                     }
-                    cache[key] = value
+                    store_in_cache(key, value)
 
                     print(command_line)
                     print(data_block)
 
                     if not value["noreply"]:
+                        print("STORED")
                         connection.sendall(b"STORED\r\n")
-                    else:
-                        connection.sendall(b"\r\n")
 
-                    buffer = remainder
+                    break
                 else:
                     buffer = command_line + '\r\n' + remainder
                     break
     finally:
         connection.close()
 
-def start_server(host='localhost', port=11211):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
+def handle_get_request(connection, key):
+    if key in cache:
+        value = cache[key]
+        if value['exptime'] != 0 and time.time() > value['exptime']:
+            del cache[key]
+            response = "END\r\n"
+        else:
+            response = f"VALUE {key} {value['flags']} {value['byte_count']}\r\n{value['data_block']}\r\nEND\r\n"
+    else:
+        response = "END\r\n"
+    connection.sendall(response.encode('utf-8'))
 
-    print(f"Server started on {host}:{port}")
+def store_in_cache(key, value):
+    if value["exptime"] <= 0:
+        value["exptime"] = time.time()
+    else:
+        value["exptime"] = time.time() + value["exptime"]
+    cache[key] = value
+
+def manage_expiry():
+    while True:
+        time.sleep(10)
+        current_time = time.time()
+        keys_to_delete = [key for key, value in cache.items() if value['exptime'] != 0 and current_time > value['exptime']]
+        for key in keys_to_delete:
+            del cache[key]
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 11211))
+    server_socket.listen()
+
+    expiry_thread = threading.Thread(target=manage_expiry)
+    expiry_thread.daemon = True
+    expiry_thread.start()
+
+    print("Server listening on port 11211")
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
+        connection, _ = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(connection,))
+        client_thread.daemon = True
+        client_thread.start()
 
 if __name__ == "__main__":
     start_server()
