9b2197264493e63d33f718abe52936777fd8f6c8
sean.mull <kimlan065@gmail.com>
2025-07-14 10:10:21 +1000

    fixed issue with storing wrong data type string instead of bytes

--- python/03-compression_tool/comp_tool.py
+++ python/03-compression_tool/comp_tool.py
@@ -1,5 +1,6 @@
 import heapq
 import pickle
+from bitarray import bitarray
 
 def create_freq_table(s):
 
@@ -11,6 +12,12 @@ def create_freq_table(s):
         else:
             freq_table[c] += 1
     return freq_table
+
+def encode_text(text, code_map):
+    encoded_text = bitarray()
+    for char in text:
+        encoded_text.extend(code_map[char])
+    return encoded_text
 
 def create_huffman_tree(freq_table):
 
@@ -50,7 +57,7 @@ def create_char_to_binary_mapping(huffman_tree):
         _ , _ , node = n
         if not node.left and not node.right:
             binary_to_char["".join(binary_string)] = node.char 
-            char_to_binary[node.char] = binary_string
+            char_to_binary[node.char] = bitarray("".join(binary_string))
         dfs(node.left, binary_string + ["0"])
         dfs(node.right, binary_string + ["1"])
 
@@ -58,37 +65,14 @@ def create_char_to_binary_mapping(huffman_tree):
 
     return char_to_binary, binary_to_char
 
-def convert_string_to_binary(s, char_to_binary):
-    bsu = []
-    for c in s:
-        bsu.append(char_to_binary[c])
+def store_bits(bits, binary_to_char, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump((binary_to_char, bits), f)
 
-    bs = []
-    for a in bsu:
-        s = "".join(a)
-        bs.append(s)
-
-    return "".join(bs)
-
-def store_bits(bits, binary_to_char, binary_to_char_filename, binary_filename):
-    encoded_bits = bits.encode('utf-8')
-
-    with open(binary_to_char_filename, 'wb') as f:
-        pickle.dump(binary_to_char, f)
-
-    with open(binary_filename, "wb") as f:
-        f.write(encoded_bits)
-
-
-def load_bits(binary_to_char_filename, binary_filename):
-
-    with open(binary_to_char_filename, 'rb') as file:
-        binary_to_char = pickle.load(file)
-
-    with open(binary_filename, "rb") as f:
-        encoded_bits = f.read()
-        decoded_bits = encoded_bits.decode('utf-8')
-        return decoded_bits, binary_to_char
+def load_bits(filename):
+    with open(filename, 'rb') as file:
+        comp_table, bits = pickle.load(file)
+        return bits.to01(), comp_table
 
 def print_file(decoded_bits, comp_table):
     bits = []
@@ -107,12 +91,11 @@ def main():
     freq_table = create_freq_table(s)
     huffman_tree = create_huffman_tree(freq_table)
     char_to_binary, binary_to_char = create_char_to_binary_mapping(huffman_tree)
-    bits = convert_string_to_binary(s, char_to_binary)
-    store_bits(bits, binary_to_char, "table.pkl", "moby_dick.bin")
-    decoded_bits, comp_table = load_bits("table.pkl", "moby_dick.bin")
+    bits = encode_text(s, char_to_binary)
+    store_bits(bits, binary_to_char, "test_string.pkl")
+    decoded_bits, comp_table = load_bits("test_string.pkl")
     print_file(decoded_bits, comp_table)
 
-    
 main()
 
 
