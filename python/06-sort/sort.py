935eb66c96ea02219ca7c46d83c69ae9127cb40f
sean.mull <kimlan065@gmail.com>
2025-07-30 15:48:56 +1000

    finishing up sort

--- python/06-sort/sort.py
+++ python/06-sort/sort.py
@@ -1,12 +1,51 @@
+from random import shuffle
+import argparse
+from util import quicksort, merge_sort, radixsort, heapsort
 
-with open("pg132.txt", "r", encoding="utf-8") as file:
+parser = argparse.ArgumentParser(
+                    prog='sort',
+                    description='Sorts text from file')
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-u', '--unique',
+                    action='store_true')  # on/off flag
+parser.add_argument('-m', '--mergesort',
+                    action='store_true')  # on/off flag
+parser.add_argument('-q', '--quicksort',
+                    action='store_true')  # on/off flag
+parser.add_argument('-e', '--heapsort',
+                    action='store_true')  # on/off flag
+parser.add_argument('-r', '--radixsort',
+                    action='store_true')  # on/off flag
+parser.add_argument('-t', '--randomsort',
+                    action='store_true')  # on/off flag
+
+
+args = parser.parse_args()
+num_of_sorts = sum([args.mergesort, args.quicksort, args.radixsort, args.randomsort, args.heapsort])
+if num_of_sorts > 1:
+    raise Exception("Choose one or none of the following sorts: --mergesort, --quicksort, --radixsort, --randomsort, --heapsort")
+
+with open(args.filename, "r", encoding="utf-8") as file:
     input_stream = file.read()
 
-unique = True
+unique = args.unique
 
 lines = input_stream.split("\n")
 if unique:
     lines = list(set(lines))
-lines.sort()
 
-pass
+if args.quicksort:
+    lines = quicksort(lines)
+elif args.mergesort:
+    lines = merge_sort(lines)
+elif args.heapsort:
+    lines = heapsort(lines)
+elif args.radixsort:
+    lines = radixsort(lines)
+elif args.randomsort:
+    lines = shuffle(lines)
+else:
+    lines = quicksort(lines)
+
+for line in lines:
+    print(line)
